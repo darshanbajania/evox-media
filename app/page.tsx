10fa84e3cbeb746e35fe4b68607e761f6e4bb8d1
darshanbajania <darshanbajania1999@gmail.com>
2024-06-26 00:08:58 +0530

    feat: who we are section

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="max-w-[1244px] mx-auto">
       <section>
-        <div className="flex h-[400px]  pt-5">
+        <div className="flex h-[500px]  pt-5">
           <div className="w-[50%] flex items-center justify-center">
             <img src="evox-bike.png" className="w-[300px]" />
           </div>
@@ -18,6 +18,20 @@ export default function Home() {
               <CallToAction label={"Get Started Today"} />
             </div>
           </div>
+        </div>
+      </section>
+      <section className="mt-[2rem]">
+        <h2 className="text-[2rem] ">Who we are</h2>
+        <div className="w-[40%]">
+          <p className="mb-2">
+            Evox Media is revolutionizing outdoor advertising by leveraging the
+            mobility of delivery vehicles.{" "}
+          </p>
+          <p className="text-[#51545C]">
+            Our delivery boxes, equipped with high-definition screens on three
+            sides, provide dynamic advertising opportunities as they travel
+            across the city.
+          </p>
         </div>
       </section>
     </div>
