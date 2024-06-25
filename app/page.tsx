a947d6764a95755e342e99db188e3c97d8afd7a1
darshanbajania <darshanbajania1999@gmail.com>
2024-06-26 00:02:24 +0530

    feat: heading section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import CallToAction from "@/components/CallToAction";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
@@ -5,8 +6,18 @@ export default function Home() {
   return (
     <div className="max-w-[1244px] mx-auto">
       <section>
-        <div>
-          <img src="" />
+        <div className="flex h-[400px]  pt-5">
+          <div className="w-[50%] flex items-center justify-center">
+            <img src="evox-bike.png" className="w-[300px]" />
+          </div>
+          <div className="flex  flex-1 flex-col justify-center">
+            <h1 className="text-[3rem] w-[80%]">
+              Transform Your Commute into Profit
+            </h1>
+            <div className="flex-start">
+              <CallToAction label={"Get Started Today"} />
+            </div>
+          </div>
         </div>
       </section>
     </div>
---
+++ components/CallToAction.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@nextui-org/react";
+import React from "react";
+
+const CallToAction = ({ label }) => {
+  return <Button variant="solid">{label}</Button>;
+};
+
+export default CallToAction;
