43f15dd6082b4e749810f1b21bc34ace0d7f77d6
darshanbajania <darshanbajania1999@gmail.com>
2024-06-26 00:28:41 +0530

    feat: earn money while you drive section

--- app/page.tsx
+++ app/page.tsx
@@ -35,9 +35,9 @@ export default function Home() {
           </p>
         </div>
       </section>
-      <section className="mt-[2rem]">
+      <section className="mt-[5rem]">
         <h2 className="text-xl text-center">How it Works?</h2>
-        <div className="flex justify-between mt-[3rem]">
+        <div className="flex justify-between mt-[5rem]">
           <HowItWorksCard
             title={"Step 1"}
             description={
@@ -57,6 +57,24 @@ export default function Home() {
             description={"Get paid for driving and advertising."}
             imagePath={"giving-payment.png"}
           />
+        </div>
+      </section>
+      <section>
+        <div className="flex h-[500px]  pt-5">
+          <div className="flex  flex-1 flex-col justify-center">
+            <h1 className="text-xl w-[80%]">Earn Money While You Drive</h1>
+            <p className="text-xl text-[#51545C] mt-4 w-[80%]">
+              Turn your delivery route into a profitable venture. Join Evox
+              Media, install our delivery box, and start earning extra income
+              effortlessly.
+            </p>
+            <div className="flex-start mt-4">
+              <CallToAction label={"Get Started Today"} />
+            </div>
+          </div>
+          <div className="w-[50%] flex items-center justify-center">
+            <img src="happy-delivery-person.png" className="w-[80%]" />
+          </div>
         </div>
       </section>
     </div>
