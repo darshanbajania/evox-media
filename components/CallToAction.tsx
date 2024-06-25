43455ab88f2dc3586cf06e3f9a9c73c12ef8cfde
darshanbajania <darshanbajania1999@gmail.com>
2024-06-26 03:00:56 +0530

    fix: cta background color

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -2,7 +2,14 @@ import { Button } from "@nextui-org/react";
 import React from "react";
 
 const CallToAction = ({ label }) => {
-  return <Button variant="solid">{label}</Button>;
+  return (
+    <Button
+      className="bg-gradient-to-r from-[#E580FF] to-[#FD80A3] text-white"
+      variant="solid"
+    >
+      {label}
+    </Button>
+  );
 };
 
 export default CallToAction;
