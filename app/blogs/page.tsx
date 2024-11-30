9eaab56308663a06cb6728139f804daa8cd5bb5e
darshanbajania <darshanbajania1999@gmail.com>
2024-11-30 13:03:54 +0530

    feat: add Blog 2 card

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -7,7 +7,16 @@ import React from "react";
 const Blogs = () => {
   return (
     <div className="max-w-[1440px] mx-auto py-[5rem] mb-[300px] xl:mb-[0px] flex flex-wrap gap-8 px-5">
-      <BlogCard />
+      <BlogCard
+        title={"How to Run Out-of-Home Advertising Efficiently"}
+        description={"Out-of-home (OOH) advertising remains a powerful tool"}
+        imagePath={"/blog-1-thumbnail.png"}
+      />
+      <BlogCard
+        title={"How to Increase Conversions Using OOH"}
+        description={" Your Brand's Bold Leap!"}
+        imagePath={"/ooh-advertising.jpg"}
+      />
     </div>
   );
 };
--- components/blogs/BlogCard.tsx
+++ components/blogs/BlogCard.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from "next/navigation";
 import React from "react";
 
-const BlogCard = () => {
+const BlogCard = ({ title, imagePath, description }) => {
   const router = useRouter();
   const handleBlogClick = (blogId: string) => {
     router.push(`/blogs/${blogId}`);
@@ -11,19 +11,12 @@ const BlogCard = () => {
       onClick={() => {
         handleBlogClick("how-to-run-out-of-home-advertising-efficiently");
       }}
-      className="border-1 border-[#DBB5E5] rounded-lg relative h-[280px] sm:w-[400px] overflow-hidden cursor-pointer"
+      className="border-1 border-[#DBB5E5] rounded-lg relative h-[250px] sm:w-[400px] overflow-hidden cursor-pointer"
     >
-      <img
-        src="/blog-1-thumbnail.png"
-        className="w-full h-[300px] object-cover"
-      />
+      <img src={imagePath} className="w-full h-[300px] object-cover" />
       <div className="absolute bottom-0 left-0 bg-white p-2 rounded-lg w-[100%]">
-        <h2 className="text-xl text-[#1E1F22] ">
-          How to Run Out-of-Home Advertising Efficiently
-        </h2>
-        <p className="mt-2 text-[#51545C] truncate ...">
-          Out-of-home (OOH) advertising remains a powerful tool
-        </p>
+        <h2 className="text-xl text-[#1E1F22] ">{title}</h2>
+        <p className="mt-2 text-[#51545C] truncate ...">{description}</p>
       </div>
     </div>
   );
