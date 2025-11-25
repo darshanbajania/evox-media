bc164253058efd8298b6b6b86098fe7c56d938f6
darshanbajania <darshanbajania1999@gmail.com>
2025-11-25 08:40:09 +0530

    fix: form links

--- utils/forms.ts
+++ utils/forms.ts
@@ -1,4 +1,6 @@
 export const formLinks = {
-  advertiserEnquiry: "https://forms.gle/egfGayK3jR3cWvox8",
-  driverEnquiry: "https://forms.gle/QSwcUosaeG7Fr4a1A",
+  // advertiserEnquiry: "https://forms.gle/egfGayK3jR3cWvox8",
+  // driverEnquiry: "https://forms.gle/QSwcUosaeG7Fr4a1A",
+  advertiserEnquiry: "https://forms.gle/oSTktuxzasWqurPq9",
+  driverEnquiry: "https://forms.gle/mP5aWHfRZ1mrfbCk8",
 };
