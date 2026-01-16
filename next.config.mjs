d29e31d6bc3f2312a5f1baab8ea7ffd05cb96ca3
darshanbajania <darshanbajania1999@gmail.com>
2026-01-16 22:52:44 +0530

    Refactor code structure for improved readability and maintainability

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  rewrites: async () => [
+    {
+      source: "/master-service-agreement",
+      destination: "/master-service-agreement.html",
+    },
+  ],
 };
 
 export default nextConfig;
