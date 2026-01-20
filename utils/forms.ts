6fbe43589dbe843a76507fd923004d133c0e0601
darshanbajania <darshanbajania1999@gmail.com>
2026-01-20 22:16:04 +0530

    fix: update driver enquiry link in formLinks

--- utils/forms.ts
+++ utils/forms.ts
@@ -2,5 +2,5 @@ export const formLinks = {
   // advertiserEnquiry: "https://forms.gle/egfGayK3jR3cWvox8",
   // driverEnquiry: "https://forms.gle/QSwcUosaeG7Fr4a1A",
   advertiserEnquiry: "https://forms.gle/oSTktuxzasWqurPq9",
-  driverEnquiry: "https://forms.gle/mP5aWHfRZ1mrfbCk8",
+  driverEnquiry: "https://tally.so/r/ODXLxa",
 };
